ddc8e8f275601a8752e11116d1274101facb8bbb
Sayed Al Zawad XEEM <sayedxeem24@gmail.com>
2021-02-14 02:58:33 +0600

    Database upload

--- RealState/App_Start/WebApiConfig.cs
+++ RealState/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace RealState
 {
@@ -20,6 +21,7 @@ namespace RealState
                 routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            config.EnableCors(new EnableCorsAttribute("*","*","*"));
         }
     }
 }
